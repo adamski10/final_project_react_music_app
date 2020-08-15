f8316b44a5dbd688698b1de03ceb04c6af06cc3b
Ahmed Hamed <tunashark@gmail.com>
2020-08-15 20:00:08 +0100

    basic frontend stuff

---
+++ frontend/src/containers/Spicify.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+class Spicify extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            null
+        )
+    }
+}
