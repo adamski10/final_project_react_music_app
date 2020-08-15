67d1cf17e24c5a68c36ad04fe94857dda3c482d1
Ahmed Hamed <tunashark@gmail.com>
2020-08-15 18:54:48 +0100

    fleshed out authorisation a bit

--- backend_node/server.js
+++ backend_node/server.js
@@ -32,29 +32,42 @@ const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI
 });
 
-const scope = ["user-read-private user-read-email playlist-modify-private"]
+const scope = ["user-library-read user-follow-modify user-read-recently-played app-remote-control user-follow-read playlist-read-private user-read-currently-playing\
+ user-read-playback-position user-top-read user-library-modify playlist-modify-public user-modify-playback-state playlist-read-collaborative user-read-private user-read-email playlist-modify-private ugc-image-upload user-read-playback-state streaming"]
 
 // const authorizeURL = spotifyApi.createAuthorizeURL(scope);
 
 app.get('/login', (req,res) => {
     const html = spotifyApi.createAuthorizeURL(scope)
     console.log(html)
-    res.send(html+"&show_dialog=true")  
+    res.redirect(html+"&show_dialog=true")  
 })
 
 app.get('/', async (req,res) => {
     const { code } = req.query;
     console.log(code)
     try {
-      const data = await spotifyApi.authorizationCodeGrant(code)
+      let data = await spotifyApi.authorizationCodeGrant(code)
       const { access_token, refresh_token } = data.body;
       spotifyApi.setAccessToken(access_token);
       spotifyApi.setRefreshToken(refresh_token);
   
       res.redirect('https://www.google.com');
-    } catch(err) {
+    } 
+    catch(err) {
       res.redirect('/#/error/invalid token');
     }
+});
+
+app.get('/playlists', async (req,res) => {
+    try {
+      let result = await spotifyApi.getUserPlaylists();
+      console.log(result.body);
+      res.status(200).send(result.body);
+    } catch (err) {
+      res.status(400).send(err)
+    }
+  
   });
 
   
