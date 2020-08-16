7588032a0e4cc5b58fc7543c90fc8e7276fc261f
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-08-16 14:10:02 +0100

    added annotations

--- frontend/src/components/Webcam.js
+++ frontend/src/components/Webcam.js
@@ -1,17 +1,23 @@
 
 
-import React from "react";
+import React, {useRef, useState, useCallback} from "react";
 import Webcam from "react-webcam";
  
 const WebcamCapture = () => {
     // This creates a blank reference that any HTML element can later use to identify itself https://reactjs.org/docs/hooks-reference.html#useref
-    // Then the <Webcam> element can identify itself by saying "ref={webcamRef}" now other functions can refer directly to that HTML component. In this case it's the Capture function 
-    const webcamRef = React.useRef(null);
-    const [imgSrc, setImgSrc] = React.useState(null);
+    // Then the <Webcam> element can identify itself by saying "ref={webcamRef}" now other functions can refer directly to that HTML component by refering to it as 'webcamRef'. In this case it's the Capture function 
+    const webcamRef = useRef(null);
+
+    // We create a useState instance and use object destructuring to create the variables 'imgSrc' and the method to set imgSrc (setImgSrc) 
+    const [imgSrc, setImgSrc] = useState(null);
   
-    const capture = React.useCallback(() => {
+    // https://www.youtube.com/watch?v=-Ls48dd-vJE when you wrap a function in useCallback you prevent the function from triggering a rerender in a .memo component or if the function in referenced in a useEffect. It doesn't change the functionality or anything like that.
+    // "Pass an inline callback and an array of dependencies. useCallback will return a memoized version of the callback that only changes if one of the dependencies has changed. This is useful when passing callbacks to optimized child components that rely on reference equality to prevent unnecessary renders (e.g. shouldComponentUpdate)."
+    const capture = useCallback(() => {
+        //we get the screenshot and set it as imageSrc
       const imageSrc = webcamRef.current.getScreenshot();
       setImgSrc(imageSrc);
+      console.log(imageSrc)
     }, [webcamRef, setImgSrc]);
   
     return (
