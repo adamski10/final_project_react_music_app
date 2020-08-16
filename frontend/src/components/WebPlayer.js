5135f036d4d994c9a4cc0a16d0952e22a0cbac8a
Ahmed Hamed <tunashark@gmail.com>
2020-08-16 21:38:42 +0100

    basic setup of web player

---
+++ frontend/src/components/WebPlayer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class spotifyWebPlayer extends Component {
+
+    constructor(props) {
+
+    }
+
+    render() {
+        return (
+            null
+        )
+    }
+
+}
