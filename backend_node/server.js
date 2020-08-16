d2a0b0beacaf215dfc83a1488c52dced2c68eac3
Ahmed Hamed <tunashark@gmail.com>
2020-08-16 21:35:58 +0100

    trying to understand the spotify api

--- backend_node/server.js
+++ backend_node/server.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: "http://localhost:3000/pleasework"
+    redirectUri: process.env.REDIRECT_URI
 });
 
 const scope = ["user-library-read user-follow-modify user-read-recently-played app-remote-control user-follow-read playlist-read-private user-read-currently-playing\
@@ -51,21 +51,57 @@ app.get('/login', (req,res) => {
     res.redirect(html+"&show_dialog=true")  
 })
 
-app.get('/', async (req,res) => {
-    const { code } = req.query;
-    console.log(code)
-    try {
-      let data = await spotifyApi.authorizationCodeGrant(code)
-      const { access_token, refresh_token } = data.body;
-      spotifyApi.setAccessToken(access_token);
-      spotifyApi.setRefreshToken(refresh_token);
+// app.get('/', async (req,res) => {
+//     const { code } = req.query;
+//     console.log(code)
+//     try {
+//       let data = await spotifyApi.authorizationCodeGrant(code)
+//       const { access_token, refresh_token } = data.body;
+//       spotifyApi.setAccessToken(access_token);
+//       spotifyApi.setRefreshToken(refresh_token);
   
-      res.redirect('http://localhost:3001/spicify');
-    } 
-    catch(err) {
-      res.redirect('/#/error/invalid token');
+//       res.redirect('http://localhost:3001/spicify');
+//     } 
+//     catch(err) {
+//       res.redirect('/#/error/invalid token');
+//     }
+// });
+
+app.get('/', (req, res) => {
+    let body1 = {
+        grant_type: "authorization_code",
+        code: req.query.code,
+        redirect_uri: process.env.REDIRECT_URI,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET
     }
-});
+    let access_token = ""
+    let refresh_token = ""
+    try {
+        fetch('https://accounts.spotify.com/api/token', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "application/json"
+            },
+            body: encodeFormData(body1)
+        })
+        .then(res => res.json())
+        .then(data => {
+            access_token = data.access_token;
+            refresh_token = data.refresh_token;
+            console.log(access_token);
+            console.log(refresh_token);
+            spotifyApi.setAccessToken(access_token);
+            spotifyApi.setRefreshToken(refresh_token);
+            let query = queryString.stringify(data);
+            res.redirect(`http://localhost:3000/#${query}`);
+            // res.json(data)
+        })
+    } catch (err) {
+        res.status(400).send(err) 
+    }
+})
 
 app.get('/playlists', async (req,res) => {
     try {
@@ -76,6 +112,57 @@ app.get('/playlists', async (req,res) => {
       res.status(400).send(err)
     }
 });
+
+app.get("/playlists/:token", async (req, res) => {
+  await fetch('https://api.spotify.com/v1/me/playlists', {
+    headers: {
+      "Authorization": `Bearer ${req.params.token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => res.json(data))
+})
+
+app.get("/playlists/:playlistid/:token", async (req, res) => {
+  await fetch(`https://api.spotify.com/v1/playlists/${req.params.playlistid}/tracks`, {
+    headers: {
+      "Authorization": `Bearer ${req.params.token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => res.json(data))
+})
+
+app.get("/user/:userid/:token", async (req, res) => {
+  await fetch(`https://api.spotify.com/v1/users/${req.params.userid}`, {
+    headers: {
+      "Authorization": `Bearer ${req.params.token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => res.json(data))
+})
+
+app.get("/track/:trackid/:token", async (req, res) => {
+  await fetch(`https://api.spotify.com/v1/tracks/${req.params.trackid}`, {
+    headers: {
+      "Authorization": `Bearer ${req.params.token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => res.json(data))
+})
+
+app.get("/track/audio_features/:trackid/:token", async (req, res) => {
+  await fetch(`https://api.spotify.com/v1/audio-features/${req.params.trackid}`, {
+    headers: {
+      "Authorization": `Bearer ${req.params.token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => res.json(data))
+})
+
 
 app.get('/:id', (req, res) => {
     const token = req.params.id;
--- frontend/src/containers/Spicify.js
+++ frontend/src/containers/Spicify.js
@@ -5,16 +5,85 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 class Spicify extends Component {
     constructor(props) {
         super(props);
+        const params = this.getHashParams();
+        const token = params.access_token;
+        console.log("HI HERE I AM ", token)
+        console.log(params);
         this.state = {
             valence: null,
-            tracks: null
+            playlists: null,
+            playlistTracks: null,
+            tracks: null,
+            userToken: params.access_token,
+            userId: null
         }
+        this.fetchMyPlaylists = this.fetchMyPlaylists.bind(this);
+        this.fetchMyPlaylistTracks = this.fetchMyPlaylistTracks.bind(this);
+        this.fetchMyTracks = this.fetchMyTracks.bind(this);
     }
+
+    fetchMyPlaylists() {
+        const baseUrl = `http://localhost:8080/playlists/${this.state.userToken}`
+        fetch(baseUrl)
+        .then(res => res.json())
+        .then(data => this.setState({
+            playlists: data
+        }))
+        console.log(this.state.playlists)
+    }
+
+    fetchMyPlaylistTracks() {
+        const promises = this.state.playlists.items.map((playlist) => {
+            return fetch(`http://localhost:8080/playlists/${playlist.id}/${this.state.userToken}`)
+                .then(res => res.json());
+            });
+
+        Promise.all(promises)
+            .then((results) => {
+                this.setState({
+                    playlistTracks: results
+                })
+            })
+        
+        console.log(this.state.playlistTracks);
+    }
+
+    fetchMyTracks() {
+        const promises = this.state.playlistTracks.map((playlist) => {
+            return playlist.items.map((tracks) => {
+                // console.log(tracks)
+                return fetch(`http://localhost:8080/track/audio_features/${tracks.track.id}/${this.state.userToken}`).then(res => res.json())
+            })
+        })
+
+        Promise.all(promises)
+            .then((results) => {
+                this.setState({
+                    tracks: results
+                })
+            })
+        console.log(this.state.tracks);
+    }
+
+    getHashParams() {
+        let hashParams = {};
+        let e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        e = r.exec(q)
+        while (e) {
+           hashParams[e[1]] = decodeURIComponent(e[2]);
+           e = r.exec(q);
+        }
+        return hashParams;
+      }
 
     render() {
         return (
             <Router>
                 <>
+                    <button onClick={this.fetchMyPlaylists}>PLAYLISTS</button>
+                    <button onClick={this.fetchMyPlaylistTracks}>PLAYLIST TRACKS</button>
+                    <button onClick={this.fetchMyTracks}>TRACK FEATURES</button>
                     <Route 
                         path="/spicify"
                         render={() => <Home />} />
