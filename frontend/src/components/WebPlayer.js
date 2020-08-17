9c759dbaaedc6796d8073fa2c4740ef75e61443d
Ahmed Hamed <tunashark@gmail.com>
2020-08-17 21:23:15 +0100

    working web player without scriptcache stuff

--- frontend/src/components/WebPlayer.js
+++ frontend/src/components/WebPlayer.js
@@ -7,16 +7,18 @@ class SpotifyWebPlayer extends Component {
         super(props)
         this.state = {
             webPlayer: null,
-            deviceId: null
+            device_id: null
         }
-        console.log(this.props.accessToken)
+        this.handleScriptError = this.handleScriptError.bind(this);
+        this.handleScriptLoad = this.handleScriptLoad.bind(this);
+        this.startPlayback = this.startPlayback.bind(this);
     }
 
-    handleScriptError = () => {
+    handleScriptError() {
         console.log("LOL GTFO, NERD")
     }
 
-    handleScriptLoad = () => {
+    handleScriptLoad() {
         window.onSpotifyWebPlaybackSDKReady = () => {
             console.log(this)
             const token = this.props.accessToken;
@@ -24,99 +26,49 @@ class SpotifyWebPlayer extends Component {
                 name: 'Web Playback SDK Quick Start Player',
                 getOAuthToken: cb => { cb(token); }
             });
+            player.connect();
             this.setState({
                 webPlayer: player
             })
-            player.addListener('ready', ({ deviceId }) => {
+            player.addListener('ready', ({ device_id }) => {
+                // console.log(device_id)
+                // this.props.handleDeviceId(device_id)
                 this.setState({
-                    deviceId: deviceId
+                    device_id: device_id
                 })
-            })
-            player.connect();
+            })  
         }
+    }
+
+    startPlayback() {
+        console.log(this.props.accessToken);
+        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${this.state.device_id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${this.props.accessToken}`
+            },
+            body: JSON.stringify({
+                "uris": ["spotify:track:43LrnQSfEZULp0nRhOqdU3", "spotify:track:5uEnSz3GJbQeI1bEhvzKyb", "spotify:track:0cJZTQ1x6ko3gbtoLKaoQe", "spotify:track:1ZUsnvMUqF0uJkhhjZlvcY"]
+            })
+        })
     }
 
     render() {
         return (
-            <Script 
-                url="https://sdk.scdn.co/spotify-player.js" 
-                onError={this.handleScriptError} 
-                onLoad={this.handleScriptLoad}
-            />
+            <>
+                <Script 
+                    url="https://sdk.scdn.co/spotify-player.js"
+                    onCreate={this.handleScriptCreate}
+                    onError={this.handleScriptError} 
+                    onLoad={this.handleScriptLoad}
+                />
+
+                <button onClick={this.startPlayback}>HIT ME BABY ONE MORE TIME</button>
+            </>
         )
     }
 
 }
 
 export default SpotifyWebPlayer;
-
-// import React, {Component} from 'react';
-// import {ScriptCache} from '../helpers/ScriptCache';
-
-// // this is horrible, blame Alex
-// let context = null
-
-// class SpotifyWebPlayer extends Component {
-
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             player: null,
-//             device_id: null
-//         }
-//         new ScriptCache([
-//             {
-//                 name: "https://sdk.scdn.co/spotify-player.js",
-//                 callback: this.spotifySDKCallback
-//             }]);
-//         this.startPlayback = this.startPlayback.bind(this);
-//         context = this
-//         this.spotifySDKCallback = this.spotifySDKCallback.bind(this);
-//     }
-
-//     spotifySDKCallback() {
-//         console.log(this)
-//         window.onSpotifyWebPlaybackSDKReady = () => {
-//             console.log(this)
-//             const token = 'BQB98kpaWXcHp1kxyTOzZM-fT-JegDWSi773IxEF_prAFfA5HUlKmxuwK6TA12G2qrhlLBa4hPu4xyqJMF6m6dmfcVqg3QR2cue1w7QAlUG4UYhc0cNN8uggIUVHup7WlHV4NOewnoFAOqZEsmcOIL56ohctzM8ERq3A6_ttUH7UuNTiRxNO_CmWIMqWH683sRwevyKLNKhdcqjDS3qhM3noxa7FNKF3p4Ez0bT9mBo5RTtjB4pcV1PVguEReRZoVTVtMxkXRcNreR8cToRgwTibGYEtG1nxD2OoOG34fzY7DaQ';
-//             const player = new window.Spotify.Player({
-//               name: 'Web Playback SDK Quick Start Player',
-//               getOAuthToken: cb => { cb(token); }
-//             });
-//             player.addListener('ready', ({ device_id }) => {
-//                 console.log(context);
-//                 context.setState({
-//                     device_id: device_id
-//                 })
-//             })
-            
-//             player.connect();
-//         }
-//     }
-
-//     startPlayback() {
-//         fetch(`https://api.spotify.com/v1/me/player/play?device_id=${this.state.device_id}`, {
-//             method: "PUT",
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 "Authorization": "Bearer BQB98kpaWXcHp1kxyTOzZM-fT-JegDWSi773IxEF_prAFfA5HUlKmxuwK6TA12G2qrhlLBa4hPu4xyqJMF6m6dmfcVqg3QR2cue1w7QAlUG4UYhc0cNN8uggIUVHup7WlHV4NOewnoFAOqZEsmcOIL56ohctzM8ERq3A6_ttUH7UuNTiRxNO_CmWIMqWH683sRwevyKLNKhdcqjDS3qhM3noxa7FNKF3p4Ez0bT9mBo5RTtjB4pcV1PVguEReRZoVTVtMxkXRcNreR8cToRgwTibGYEtG1nxD2OoOG34fzY7DaQ"
-//             },
-//             body: JSON.stringify({
-//                 "uris": ["spotify:track:43LrnQSfEZULp0nRhOqdU3", "spotify:track:5uEnSz3GJbQeI1bEhvzKyb"]
-//             })
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <>
-//                 <h3>Spotify</h3>
-//                 <button onClick={this.spotifySDKCallback}>Connect</button>
-//                 <button onClick={this.startPlayback}>Play</button>
-//             </>
-//         )
-//     }
-
-// }
-
-// export default SpotifyWebPlayer;
