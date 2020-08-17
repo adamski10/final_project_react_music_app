dfa8ed97f060ed5367649a884990a75e819cd087
Ahmed Hamed <tunashark@gmail.com>
2020-08-17 21:32:08 +0100

    pause/stop functionality working

--- frontend/src/components/WebPlayer.js
+++ frontend/src/components/WebPlayer.js
@@ -7,15 +7,18 @@ class SpotifyWebPlayer extends Component {
         super(props)
         this.state = {
             webPlayer: null,
+            playerResume: null,
+            playerPause: null,
             device_id: null
         }
         this.handleScriptError = this.handleScriptError.bind(this);
         this.handleScriptLoad = this.handleScriptLoad.bind(this);
+        this.pausePlayback = this.pausePlayback.bind(this);
         this.startPlayback = this.startPlayback.bind(this);
     }
 
     handleScriptError() {
-        console.log("LOL GTFO, NERD")
+        console.log("GTFO, NERD")
     }
 
     handleScriptLoad() {
@@ -40,8 +43,16 @@ class SpotifyWebPlayer extends Component {
         }
     }
 
+    pausePlayback() {
+        this.state.webPlayer.pause()
+        .then(() => {
+            this.setState({
+                playerPause: true
+            })
+        })
+    }
+
     startPlayback() {
-        console.log(this.props.accessToken);
         fetch(`https://api.spotify.com/v1/me/player/play?device_id=${this.state.device_id}`, {
             method: "PUT",
             headers: {
@@ -63,7 +74,7 @@ class SpotifyWebPlayer extends Component {
                     onError={this.handleScriptError} 
                     onLoad={this.handleScriptLoad}
                 />
-
+                <button onClick={this.pausePlayback}>DON'T STOP ME NOW</button>
                 <button onClick={this.startPlayback}>HIT ME BABY ONE MORE TIME</button>
             </>
         )
