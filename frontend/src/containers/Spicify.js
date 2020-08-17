ce12f3857b15a8a42faf76527deb4b7a4828d8a5
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-08-17 13:57:51 +0100

    Add login page with button to backend auth route

--- frontend/src/containers/Spicify.js
+++ frontend/src/containers/Spicify.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Home from '../components/Home';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from "../components/Login";
 
 
 
@@ -9,14 +10,25 @@ class Spicify extends Component {
         super(props);
         this.state = {
             valence: null,
-            tracks: null
+            tracks: null,
+            userSongs: []
         }
     }
+
+    componentDidMount() {
+        const url = "http://localhost:8080/songs"
+
+        fetch(url)
+        .then(res => res.json())
+        .then(userSongs => this.setState({ userSongs }))
+        .catch(err => console.error)
+    };
 
     render() {
         return (
             <Router>
                 <>
+                    <Route exact path="/" component={Login}/>
                     <Route 
                         path="/spicify"
                         render={() => <Home />} />
