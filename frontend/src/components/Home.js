6d2671404c8bf3a16dd270073c5ea1c1e837c2f9
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-08-19 20:48:33 +0100

    Styled generate playlist button

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,7 +5,8 @@ import MoodTracker from './MoodTracker.js';
 import PlayBar from './PlayBar.js';
 import PlayList from './PlayList.js';
 import SpotifyWebPlayer from './WebPlayer';
-import mygif2003 from '../Images/coffee.gif'
+import mygif2003 from '../Images/coffee.gif';
+import "./Home.css"
 
 class Home extends Component {
 
@@ -33,7 +34,9 @@ class Home extends Component {
                 setSliderDanciness={this.props.setSliderDanciness}
                 setSliderEnergy={this.props.setSliderEnergy}
                 />
-                <button onClick={this.handleGeneratePlaylist}>Generate Playlist</button>
+                <div className="button-wrap">
+                    <button className="generate-button" onClick={this.handleGeneratePlaylist}>Generate Playlist</button>
+                </div>
                 <SpotifyWebPlayer
                     selectedSongUri={this.props.selectedSongUri}
                     accessToken={this.props.accessToken}
