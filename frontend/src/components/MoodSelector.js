f03bb47b24c5eead4a77dc385197eefdeab588cc
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-08-19 12:37:21 +0100

    added camera photo logic

--- frontend/src/components/MoodSelector.js
+++ frontend/src/components/MoodSelector.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect, Component} from 'react';
 import WebcamCapture from './Webcam';
 import CircularSlider from '@fseehawer/react-circular-slider';
-import { ReactComponent as EmojiIcon } from '../Images/smileyface.svg';
 
 import { ReactComponent as Emoji1f62d } from '../Images/svg_emojis/1f62d.svg'
 import { ReactComponent as Emoji1f622 } from '../Images/svg_emojis/1f622.svg'
@@ -45,50 +44,39 @@ import { ReactComponent as Emoji1f929 } from '../Images/svg_emojis/1f929.svg'
 
 
 
-const MoodSelector = (props, { emotion } = props) => {    
+const MoodSelector = (props) => {    
 
     const [sliderEmoji, setSliderEmoji] = useState(<Emoji1f929 />);
     useEffect( () => { changeEmoji()}, [ props.emotion ] );
 
-    useEffect(()=>{ console.log("i'm changed")},[sliderEmoji])
-
     const changeEmoji = () =>{
         switch (true) { //matches to any statement that is true
             case (props.emotion < 0.11):
-                setSliderEmoji(<Emoji1f62d className="emoji"/>)
-                console.log("i'm under 0.11 😭 1f62d")
+                setSliderEmoji(<Emoji1f62d/>)
                 break;
             case (props.emotion < 0.22):
-                console.log("i'm under 😢 1f622")
-                setSliderEmoji(<Emoji1f622 className="emoji"/>)
+                setSliderEmoji(<Emoji1f622/>)
                 break;
             case (props.emotion < 0.33):
-                console.log("i'm under 0.33 😞 1f61e")
-                setSliderEmoji(<Emoji1f61e className="emoji"/>)
+                setSliderEmoji(<Emoji1f61e/>)
                 break;
             case (props.emotion < 0.44):
-                console.log("i'm under 0.44 🙁 1f641")
-                setSliderEmoji(<Emoji1f641 className="emoji"/>)
+                setSliderEmoji(<Emoji1f641/>)
                 break;     
             case (props.emotion < 0.55):
-                console.log("i'm under 0.55 😐 1f610")
-                setSliderEmoji(<Emoji1f610 style={{filter: "drop-shadow(30px 10px 4px #db4c28)"}}/>)
+                setSliderEmoji(<Emoji1f610/>)
                 break;
             case (props.emotion < 0.66):
-                console.log("i'm under 0.66 🙂 1f642")
-                setSliderEmoji(<Emoji1f642 className="emoji"/>)
+                setSliderEmoji(<Emoji1f642/>)
                 break;
             case (props.emotion < 0.77):
-                console.log("i'm under 0.77 😀 1f600")
-                setSliderEmoji(<Emoji1f600 className="emoji"/>)
+                setSliderEmoji(<Emoji1f600/>)
                 break;
             case (props.emotion < 0.88):
-                console.log("i'm under 0.88 😁 1f601")
-                setSliderEmoji(<Emoji1f601 className="emoji"/>)
+                setSliderEmoji(<Emoji1f601/>)
                 break;
             case (props.emotion <= 1):
-                console.log("i'm under 1 🤩 1f929")
-                setSliderEmoji(<Emoji1f929 className="emoji"/>)
+                setSliderEmoji(<Emoji1f929/>)
                 break;
             default:
                 console.log("i'm default")
@@ -100,8 +88,9 @@ const MoodSelector = (props, { emotion } = props) => {
 
     return (
         <>
-           
-                <CircularSlider
+            <div className="webcam_slider_container">
+                <CircularSlider className="ring_slider"
+                    width="280"
                     label="Mood selector"
                     labelColor="#005a58"
                     knobColor="#005a58"
@@ -112,10 +101,10 @@ const MoodSelector = (props, { emotion } = props) => {
                     trackSize={15}
                     hideLabelValue="true"
                     min={0}
-                    max={1000} // the slider reacts to emotion change with min=0 and max=1 but start point is not correct
+                    max={100} // the slider reacts to emotion change with min=0 and max=1 but start point is not correct
                     data= {[]}    
-                    dataIndex={props.emotion*1000}
-                    onChange={ value => {props.setSliderValence(value/1000)} }   
+                    dataIndex={props.emotion*100}
+                    onChange={ value => {props.setSliderValence(value/100)} }   
                 >
                     {sliderEmoji}
                     {/* <sliderEmoji x="9" y="9" width="18px" height="18px" /> */}
@@ -123,6 +112,8 @@ const MoodSelector = (props, { emotion } = props) => {
                 <div className="webcam">
                     <WebcamCapture setEmotion={props.setEmotion}/>
                 </div>
+            </div>
+
                 <div className="slide-bars-container">
                     <div className="bar-energy">Energy</div>
                     <div className="range-wrap">
