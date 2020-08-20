9a70a3457566a9253781a5bb941d9b6e35e79f0b
AdamK <44851323+adamski10@users.noreply.github.com>
2020-08-20 12:25:07 +0100

    changed playlist styling

--- frontend/src/components/Track.js
+++ frontend/src/components/Track.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import "./Track.css"
-import PlayIcon from "../Images/play_icon_logo_gradient_transp.png"
+import PlayIcon from "../Images/play_icon_black.png"
 
 class Track extends Component {
 
